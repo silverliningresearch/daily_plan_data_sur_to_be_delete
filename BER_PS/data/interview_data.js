87aaa4a32afe0e39c347af084356546229f3beb2
unknown <lb@silverliningresearch.com>
2023-08-25 20:55:02 +0000

    update new data

--- BER_PS/data/interview_data.js
+++ BER_PS/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "Mailand (LIN)",
         "AirlineCode": "easyJet Europe",
         "InterviewerID": 10,
-        "download_time": "25-08-2023 21:59:21"
+        "download_time": "25-08-2023 22:54:30"
     },
     {
         "InterviewId": "d375e29f-9154-46b5-912e-1142a35aeacf",
--- CPH_Route/data/interview_data.js
+++ CPH_Route/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "InterviewerID": 999,
-        "download_time": "25-08-2023 21:59:52"
+        "download_time": "25-08-2023 22:55:01"
     },
     {
         "InterviewId": "7c295d71-9010-4846-9617-003c1537e57a",
