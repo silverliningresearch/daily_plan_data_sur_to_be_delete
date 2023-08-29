3d706a5c2adfc801c706079809b209e7ba96deed
unknown <lb@silverliningresearch.com>
2023-08-29 20:15:04 +0000

    update new data

--- BER_PS/data/completed_by_interviewer.js
+++ BER_PS/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-03-25",
         "completed_interviews": 56,
-        "download_time": "29-08-2023 21:19:35"
+        "download_time": "29-08-2023 22:14:31"
     },
     {
         "InterviewerID": 1,
--- CAG_AirHub/data/interview_data.js
+++ CAG_AirHub/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "ICN",
         "AirlineCode": "SQ",
         "InterviewerID": "",
-        "download_time": "30-08-2023 03:17:20"
+        "download_time": "30-08-2023 04:12:16"
     },
     {
         "InterviewId": "f159ba3c-e62a-4798-bf1e-10603685081d",
