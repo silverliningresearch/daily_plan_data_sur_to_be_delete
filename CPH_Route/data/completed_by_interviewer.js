ed76e10e2c980bce979987770d21f7230dcc9437
unknown <lb@silverliningresearch.com>
2023-09-07 08:37:48 +0000

    update new data

--- CPH_Route/data/completed_by_interviewer.js
+++ CPH_Route/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-06-01",
         "completed_interviews": 25,
-        "download_time": "07-09-2023 09:42:28"
+        "download_time": "07-09-2023 10:37:47"
     },
     {
         "InterviewerID": 1,
--- CPH_Route/data/interview_data.js
+++ CPH_Route/data/interview_data.js
@@ -6,7 +6,7 @@ let interview_data_raw = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "InterviewerID": 999,
-        "download_time": "07-09-2023 09:42:28"
+        "download_time": "07-09-2023 10:37:47"
     },
     {
         "InterviewId": "7c295d71-9010-4846-9617-003c1537e57a",
