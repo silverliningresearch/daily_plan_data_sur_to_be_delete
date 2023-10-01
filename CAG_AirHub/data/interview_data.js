5bc12776e66349bc5f3268a130837444e532b575
unknown <lb@silverliningresearch.com>
2023-10-01 21:08:37 +0000

    update new data

--- CAG_AirHub/data/interview_data.js
+++ CAG_AirHub/data/interview_data.js
@@ -7,7 +7,7 @@ let interview_data_raw = `[
         "Dest": "ICN",
         "AirlineCode": "SQ",
         "InterviewerID": "",
-        "download_time": "02-10-2023 04:12:44"
+        "download_time": "02-10-2023 05:07:52"
     },
     {
         "InterviewId": "f159ba3c-e62a-4798-bf1e-10603685081d",
