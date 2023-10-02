3e2371da3d8683c02fc3f71a525c99e00c1108ef
unknown <lb@silverliningresearch.com>
2023-10-02 21:53:38 +0000

    update new data

--- CPH_Route/data/completed_by_interviewer.js
+++ CPH_Route/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-06-01",
         "completed_interviews": 25,
-        "download_time": "02-10-2023 22:58:21"
+        "download_time": "02-10-2023 23:53:38"
     },
     {
         "InterviewerID": 1,
