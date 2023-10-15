2249c12e0254186c000b6dcb68c6675d302c6fd9
unknown <lb@silverliningresearch.com>
2023-10-15 05:02:38 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "",
         "Interview_Date": "2023-04-01",
         "completed_interviews": 196,
-        "download_time": "15-10-2023 12:07:25"
+        "download_time": "15-10-2023 13:02:00"
     },
     {
         "InterviewerID": "",
@@ -778,7 +778,7 @@ let completed_by_interviewer = `[
     {
         "InterviewerID": "",
         "Interview_Date": "2023-10-15",
-        "completed_interviews": 24
+        "completed_interviews": 27
     }
 ]    
 
--- CPH_Route/data/completed_by_interviewer.js
+++ CPH_Route/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": 1,
         "Interview_Date": "2023-06-01",
         "completed_interviews": 25,
-        "download_time": "15-10-2023 06:08:02"
+        "download_time": "15-10-2023 07:02:37"
     },
     {
         "InterviewerID": 1,
