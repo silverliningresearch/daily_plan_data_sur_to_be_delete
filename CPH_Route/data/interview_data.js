fdb3f0beb8764222df92c376608b5cff49efc3ed
unknown <lb@silverliningresearch.com>
2023-11-26 23:44:39 +0000

    update new data

--- CPH_Route/data/interview_data.js
+++ CPH_Route/data/interview_data.js
@@ -4,7 +4,7 @@ let interview_data_raw = `[
         "InterviewState": "Completed",
         "Dest": "AMS",
         "AirlineCode": "KL",
-        "download_time": "26-11-2023 23:49:39"
+        "download_time": "27-11-2023 00:44:32"
     },
     {
         "InterviewEndDate": "2023-05-27",
