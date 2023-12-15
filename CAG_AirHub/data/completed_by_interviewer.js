d8a6183e17cbbf32def6039967dd931143990ece
unknown <lb@silverliningresearch.com>
2023-12-15 15:05:32 +0000

    update new data

--- CAG_AirHub/data/completed_by_interviewer.js
+++ CAG_AirHub/data/completed_by_interviewer.js
@@ -3,7 +3,7 @@ let completed_by_interviewer = `[
         "InterviewerID": "",
         "Interview_Date": "2023-04-01",
         "completed_interviews": 196,
-        "download_time": "15-12-2023 22:10:18"
+        "download_time": "15-12-2023 23:04:04"
     },
     {
         "InterviewerID": "",
