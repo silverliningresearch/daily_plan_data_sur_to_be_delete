293524c183e6c4510279de103830239fca6c64a9
unknown <lb@silverliningresearch.com>
2023-12-16 03:55:11 +0000

    update new data

--- CPH_Route/data/interview_statistics.js
+++ CPH_Route/data/interview_statistics.js
@@ -4,7 +4,7 @@ let interview_statistics = `[
         "Dest": "AMS",
         "AirlineCode": "KL",
         "completed_interviews": 3,
-        "download_time": "16-12-2023 04:01:13"
+        "download_time": "16-12-2023 04:55:03"
     },
     {
         "Interview_Date": "2023-05-28",
